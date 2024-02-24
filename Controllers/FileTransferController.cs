634669ec61c6539f14b5489f48b5f978bf0f4eac
xingxing520 <46059349+xingxing2008@users.noreply.github.com>
2024-02-24 13:11:00 +0800

    添加项目文件。

---
+++ Controllers/FileTransferController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.IO.Compression;
+using System.Net.Http.Headers;
+using static FileTranserProxy.FileTransfer;
+
+namespace FileTranserProxy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FileTransferController : ControllerBase
+    {
+        private static HttpClient Client { get; } = new HttpClient();
+        [HttpPost]
+        public IActionResult Post([FromBody][FromForm] string body)
+        {
+            var filenamesAndUrls = JsonConvert.DeserializeObject <Dictionary<string,string>>(body);
+
+            return new FileCallbackResult(new MediaTypeHeaderValue("application/octet-stream"), async (outputStream, _) =>
+            {
+                using (var zipArchive = new ZipArchive(new WriteOnlyStreamWrapper(outputStream), ZipArchiveMode.Create))
+                {
+                    foreach (var kvp in filenamesAndUrls)
+                    {
+                        var zipEntry = zipArchive.CreateEntry(kvp.Key);
+                        using (var zipStream = zipEntry.Open())
+                        using (var stream = await Client.GetStreamAsync(kvp.Value))
+                            await stream.CopyToAsync(zipStream);
+                    }
+                }
+            })
+            {
+                FileDownloadName = "Res.zip"
+            };
+        }
+    }
+}
---
+++ FileTransfer.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace FileTranserProxy
+{
+    public class FileTransfer
+    {
+        public class FileCallbackResult : FileResult
+        {
+            private Func<Stream, ActionContext, Task> _callback;
+
+            public FileCallbackResult(MediaTypeHeaderValue contentType, Func<Stream, ActionContext, Task> callback)
+                : base(contentType?.ToString())
+            {
+                if (callback == null)
+                    throw new ArgumentNullException(nameof(callback));
+                _callback = callback;
+            }
+
+            public override Task ExecuteResultAsync(ActionContext context)
+            {
+                if (context == null)
+                    throw new ArgumentNullException(nameof(context));
+                var executor = new FileCallbackResultExecutor(context.HttpContext.RequestServices.GetRequiredService<ILoggerFactory>());
+                return executor.ExecuteAsync(context, this);
+            }
+
+            private sealed class FileCallbackResultExecutor : FileResultExecutorBase
+            {
+                public FileCallbackResultExecutor(ILoggerFactory loggerFactory)
+                    : base(CreateLogger<FileCallbackResultExecutor>(loggerFactory))
+                {
+                }
+
+                public Task ExecuteAsync(ActionContext context, FileCallbackResult result)
+                {
+                    SetHeadersAndLog(context, result,null,true);
+                    return result._callback(context.HttpContext.Response.Body, context);
+                }
+            }
+        }
+
+        public class fileinfo
+        {
+            public string Name { get; set; }
+            public string url { get; set; }
+        }
+    }
+}
